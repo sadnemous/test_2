beb6e579a1375e6e4efa4025adbf98d608398c98
U-GLOBAL\n074800 <n074800@L0000936.global.ual.com>
2022-07-08 13:15:24 -0500

    Commit# S1

--- test.c
+++ test.c
@@ -1,15 +1,15 @@
 /* strncpy example */
 #include <stdio.h>
 #include <string.h>
-
+#define MAX_SZ 40
 int main ()
 {
   char str1[]= "To be or not to be";
-  char str2[40];
+  char str2[MAX_SZ];
   char str3[40];
 
   /* copy to sized buffer (overflow safe): */
-  strncpy ( str2, str1, sizeof(str2) );
+  strncpy ( str2, str1, MAX_SZ );
 
   /* partial copy (only 5 chars): */
   strncpy ( str3, str2, 5 );
